639f0004ca7ddd3a1911ab6c4d670b75b9964fae
swetang24 <swetang@csu.fullerton.edu>
2018-02-28 12:07:09 -0800

    Add files via upload

---
+++ env.bat
@@ -0,0 +1,10 @@
+REM JJ
+REM SET PROJ=C:\Documents and Settings\jjohnson\Desktop\jetec\coderay.IMS5000
+
+REM DIXON
+REM LOTS OF STUFF ON PATH MESSES UP BUILD SO HERE IS A NEW ONE
+
+PATH=C:\WINDOWS\SYSTEM32;C:\WINDOWS;C:\WINDOWS\SYSTEM32\WBEM;C:\WINDOWS\SYSTEM32\WINDOWSPOWERSHELL\V1.0;C:\PROGRAM FILES\QUICKTIME\QTSYSTEM\;M:\MACOLA70\INSTALL\VBA;C:\Program Files\Common Files\Macola Shared;C:\Documents and Settings\Admin\Local Settings\Application Data\Smartbar\Application\;C:\Program Files\TortoiseSVN\bin;J:\DXNProj2701
+SET PROJ=C:\Documents and Settings\Admin\Desktop\checkout.IMS5000
+SET LIB=C:\Program Files\Microsoft Visual Studio 10.0\VC\lib
+SET INCLUDE=C:\Program Files\Microsoft Visual Studio 10.0\VC\include
